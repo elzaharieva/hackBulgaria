60a11afdd3e4f402a49f772cd9fcac2e048d2e4e
Elena <el.zaharieva28@gmail.com>
2015-04-30 03:21:42 +0300

    makingHistogramFromServers eddited

--- week7/1 - Scan-Bg-Web/makingHistogramFromServers.py
+++ week7/1 - Scan-Bg-Web/makingHistogramFromServers.py
@@ -8,17 +8,13 @@ def reading_from_histogram_file():
     path = "histogram.txt"
     f = open(path, 'r')
     text = f.read().split('\n')
-    text.remove(text[163])
-    text.remove(text[430])
-    text.remove(text[1127])
-    print (text)
     f.close()
     for i in range(0, len(text)):
-        if count_substrings(text[i],"Apache") > 0:
+        if count_substrings(text[i], "Apache") > 0:
             h.add("Apache")
-        elif count_substrings(text[i],"IIS") > 0 or count_substrings(text[i], "Microsoft") > 0:
+        elif count_substrings(text[i], "IIS") > 0 or count_substrings(text[i], "Microsoft") > 0:
             h.add("IIS")
-        elif count_substrings(text[i],"nginx") > 0:
+        elif count_substrings(text[i], "nginx") > 0:
             h.add("nginx")
         elif count_substrings(text[i], "light") > 0:
             h.add("lighttpd")
